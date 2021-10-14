06956144cc95d00ae217ce14e12e12cd99bae2fb
Shashank Chikattimala <shashankcm90@gmail.com>
2021-10-14 12:48:49 -0500

    Renamed the context names

--- src/makeStoreHook.js
+++ src/makeStoreHook.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 export default function makeStore(reducer, initialState) {
-  const storeContext = React.createContext();
-  const dispatchContext = React.createContext();
+  const StoreContext = React.createContext();
+  const DispatchContext = React.createContext();
 
   const StoreProvider = ({ children }) => {
     const [store, dispatch] = React.useReducer(reducer, initialState);
     return (
-      <dispatchContext.Provider value={dispatch}>
-        <storeContext.Provider value={store}>{children}</storeContext.Provider>
-      </dispatchContext.Provider>
+      <DispatchContext.Provider value={dispatch}>
+        <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+      </DispatchContext.Provider>
     );
   };
 
   function useStore() {
-    return React.useContext(storeContext);
+    return React.useContext(StoreContext);
   }
 
   function useDispatch() {
-    return React.useContext(dispatchContext);
+    return React.useContext(DispatchContext);
   }
 
   return [StoreProvider, useDispatch, useStore];
